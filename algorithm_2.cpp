8a9e52d13b40108753e6e35fde659b04d6132caf
kingqiuol <1491716282@qq.com>
2019-12-24 15:33:23 +0800

    first commit

--- algorithm_2.cpp
+++ algorithm_2.cpp
@@ -1315,11 +1315,41 @@ vector<vector<int>> largeGroupPositions(string S) {
 			flag=false;
 			iCount=0;
 		}	
-		cout<<i+1<<","<<iCount<<endl;
 	}
 
 	return vec;
 }
+
+/*
+832. 翻转图像
+
+给定一个二进制矩阵 A，我们想先水平翻转图像，然后反转图像并返回结果。
+水平翻转图片就是将图片的每一行都进行翻转，即逆序。例如，水平翻转 [1, 1, 0] 的结果是 [0, 1, 1]。
+反转图片的意思是图片中的 0 全部被 1 替换， 1 全部被 0 替换。例如，反转 [0, 1, 1] 的结果是 [1, 0, 0]。
+
+示例 1:
+输入: [[1,1,0],[1,0,1],[0,0,0]]
+输出: [[1,0,0],[0,1,0],[1,1,1]]
+解释: 首先翻转每一行: [[0,1,1],[1,0,1],[0,0,0]]；
+     然后反转图片: [[1,0,0],[0,1,0],[1,1,1]]
+*/
+vector<vector<int>> flipAndInvertImage(vector<vector<int>>& A) {
+	    if (A.empty() || A[0].empty()) return A;
+        int R = A.size();
+        int C = A[0].size();
+        for (int i = 0; i < R; ++i) {
+            int l = 0;
+            int r = C - 1;
+            while (l < r) {
+                swap(A[i][l], A[i][r]);
+                A[i][l++] ^= 1;
+                A[i][r--] ^= 1;
+            }
+            if (C & 1) A[i][C >> 1] ^= 1;
+        }
+        return A;
+}
+
 
 
 int main(){
